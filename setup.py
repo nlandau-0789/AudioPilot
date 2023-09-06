a668d33bfecca1fe88016141c3104b5e52e3d92e
Nathan Landau <nathan.landau1234@gmail.com>
2023-09-06 21:09:47 +0200

    fixed cx_freeze setup

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from cx_Freeze import setup, Executable
+import sys
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
 
 setup(
     name="AudioPilot",
     version="1.0",
     description="A noice audio player",
     # executables=[Executable(script="main.py", icon = 'favicon.ico')], 
-    executables=[Executable(script="main.py", base = "Win32GUI", icon = 'favicon.ico')],
+    executables=[Executable(script="main.py", base = base, icon = 'favicon.ico')],
     options={
         "build_exe": {
             "packages": ["eyed3", "multiprocessing"],
